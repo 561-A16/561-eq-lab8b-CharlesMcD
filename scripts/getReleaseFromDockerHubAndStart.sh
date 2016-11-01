51b60540dbc08e85994f3f3f1e7656e472a3d106
Charles MacDuff <Charles_5234@hotmail.com>
2016-11-01 15:13:54 -0400

    remise

--- scripts/getReleaseFromDockerHubAndStart.sh
+++ scripts/getReleaseFromDockerHubAndStart.sh
@@ -3,4 +3,4 @@ set –v
 docker pull charlesmcd/webapp
 docker stop charlesmcd/webapp
 docker rm charlesmcd/webapp
-docker run -itd -p 8025:5000 charlesmcd/webapp
+docker run -itd -p 6506:5000 charlesmcd/webapp
